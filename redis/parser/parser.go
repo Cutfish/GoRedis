f8adab7ecbc2d4558e3ccd49a960c4ab7c08101d
fishqiudao <fishqiudao@gmail.com>
2025-06-18 10:20:21 +0800

    [WN]实现协议解析器

---
+++ redis/parser/parser.go
@@ -0,0 +1,213 @@
+package parser
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"go-redis/interface/redis"
+	"go-redis/lib/logger"
+	"go-redis/redis/protocol"
+	"io"
+	"runtime/debug"
+	"strconv"
+	"strings"
+)
+
+// Payload存储redis.Reply或者是error
+type Payload struct {
+	Data redis.Reply
+	Err  error
+}
+
+// ParseStream通过io.Reader读取数据并将结果通过channel返回给调用者
+// 流式处理的接口适合供客户端/服务端使用
+func ParseStream(reader io.Reader) <-chan *Payload {
+	ch := make(chan *Payload)
+	go parse0(reader, ch)
+	return ch
+}
+
+// ParseOne解析[]byte 并返回redis.Reply
+func ParseOne(data []byte) (redis.Reply, error) {
+	ch := make(chan *Payload)
+	reader := bytes.NewReader(data)
+	go parse0(reader, ch)
+	Payload := <-ch //parse0会关闭channel
+	if Payload == nil {
+		return nil, errors.New("no reply")
+	}
+	return Payload.Data, Payload.Err
+}
+
+func parse0(rawReader io.Reader, ch chan<- *Payload) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error(err, string(debug.Stack()))
+		}
+	}()
+	reader := bufio.NewReader(rawReader)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			ch <- &Payload{Err: err}
+			close(ch)
+			return
+		}
+		length := len(line)
+		if length <= 2 || line[length-2] != '\r' {
+			// there are some empty lines within replication traffic, ignore this error
+			//protocolError(ch, "empty line")
+			continue
+		}
+		line = bytes.TrimSuffix(line, []byte{'\r', '\n'})
+		switch line[0] {
+		case '+': //简单字符串
+			content := string(line[1:])
+			ch <- &Payload{
+				Data: protocol.NewStatusReply(content),
+			}
+			// TODO 不太懂这里是在做什么
+			if strings.HasPrefix(content, "FULLRESYNC") {
+				err = parseRDBBulkString(reader, ch)
+				if err != nil {
+					ch <- &Payload{Err: err}
+					close(ch)
+					return
+				}
+			}
+		case '-': // 错误
+			ch <- &Payload{
+				Data: protocol.NewErrReply(string(line[1:])),
+			}
+		case ':': //整数
+			value, err := strconv.ParseInt(string(line[1:]), 10, 64)
+			if err != nil {
+				protocolError(ch, "illegal number: "+string(line[1:]))
+				continue
+			}
+			ch <- &Payload{
+				Data: protocol.NewIntReply(value),
+			}
+		case '$': //复杂字符串
+			err = parseBulkString(line, reader, ch)
+			if err != nil {
+				ch <- &Payload{Err: err}
+				close(ch)
+				return
+			}
+		case '*': //数组
+			err = parseArray(line, reader, ch)
+			if err != nil {
+				ch <- &Payload{Err: err}
+				close(ch)
+				return
+			}
+		default:
+			args := bytes.Split(line, []byte{' '})
+			ch <- &Payload{
+				Data: protocol.NewMultiBulkReply(args),
+			}
+		}
+
+	}
+}
+
+// 解析复杂字符串
+func parseBulkString(header []byte, reader *bufio.Reader, ch chan<- *Payload) error {
+	strLen, err := strconv.ParseInt(string(header[1:]), 10, 64)
+	if err != nil || strLen < -1 {
+		protocolError(ch, "illegal bulk string header: "+string(header))
+		return nil
+	} else if strLen == -1 {
+		ch <- &Payload{
+			Data: protocol.NewNullBulkReply(),
+		}
+		return nil
+	}
+	body := make([]byte, strLen+2)
+	_, err = io.ReadFull(reader, body)
+	if err != nil {
+		return err
+	}
+	ch <- &Payload{
+		Data: protocol.NewBulkReply(body[:len(body)-2]),
+	}
+	return nil
+}
+
+// 解析数组
+func parseArray(header []byte, reader *bufio.Reader, ch chan<- *Payload) error {
+	nStrs, err := strconv.ParseInt(string(header[1:]), 10, 64)
+	if err != nil || nStrs < 0 {
+		protocolError(ch, "illegal array header "+string(header[1:]))
+		return nil
+	} else if nStrs == 0 {
+		ch <- &Payload{
+			Data: protocol.NewEmptyMultiBulkReply(),
+		}
+		return nil
+	}
+	lines := make([][]byte, 0, nStrs)
+	for i := int64(0); i < nStrs; i++ {
+		var line []byte
+		line, err = reader.ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+		length := len(line)
+		if length < 4 || line[length-2] != '\r' || line[0] != '$' {
+			protocolError(ch, "illegal bulk string header "+string(line))
+			break
+		}
+		strLen, err := strconv.ParseInt(string(line[1:length-2]), 10, 64)
+		if err != nil || strLen < -1 {
+			protocolError(ch, "illegal bulk string length "+string(line))
+			break
+		} else if strLen == -1 {
+			// TODO 查看修改的是否有问题
+			lines = append(lines, nil)
+			// lines = append(lines, []byte{})
+		} else {
+			body := make([]byte, strLen+2)
+			_, err := io.ReadFull(reader, body)
+			if err != nil {
+				return err
+			}
+			lines = append(lines, body[:len(body)-2])
+		}
+	}
+	ch <- &Payload{
+		Data: protocol.NewMultiBulkReply(lines),
+	}
+	return nil
+}
+
+// there is no CRLF between RDB and following AOF, therefore it needs to be treated differently
+func parseRDBBulkString(reader *bufio.Reader, ch chan<- *Payload) error {
+	header, err := reader.ReadBytes('\n')
+	if err != nil {
+		return errors.New("failed to read bytes")
+	}
+	header = bytes.TrimSuffix(header, []byte{'\r', '\n'})
+	if len(header) == 0 {
+		return errors.New("empty header")
+	}
+	strLen, err := strconv.ParseInt(string(header[1:]), 10, 64) //10进制 64位整数
+	if err != nil || strLen <= 0 {
+		return errors.New("illegal bulk header: " + string(header))
+	}
+	body := make([]byte, strLen)
+	_, err = io.ReadFull(reader, body)
+	if err != nil {
+		return err
+	}
+	ch <- &Payload{
+		Data: protocol.NewBulkReply(body[:len(body)]),
+	}
+	return nil
+}
+
+func protocolError(ch chan<- *Payload, msg string) {
+	err := errors.New("protocol error: " + msg)
+	ch <- &Payload{Err: err}
+}
